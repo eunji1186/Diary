b182c0dfd43ec2fec9b5ecfb66b7fe889d31f37b
eunji1186 <eunji1186@kakao.com>
2022-06-05 16:27:38 +0900

    header 작성

---
+++ .vscode/main.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <meta charset=utf-8>
+    <title>Exchange Diary</title>
+</head>
+
+<body>
+    <!--젤 처음 화면이 떴을 때 보여줄 화면 구성
+        예를 들면 최근 게시글, 베스트 게시글 등등
+    -->
+    <?php echo "main"; ?>
+
+</body>
+
+</html>
---
+++ .vscode/total.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>교환 일기</title>
+    <link rel="stylesheet" type="text/css" href="./css/common.css">
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+</head>
+
+<body>
+    <header>
+        <?php include "header.php"; ?>
+    </header>
+
+    <section>
+        <?php include "main.php"; ?>
+        <hr>
+    </section>
+
+    <footer>
+        <?php include "footer.php"; ?>
+    </footer>
+</body>
+
+</html>
